0c0e86d101b0ff0ef7ec2a5e1538bfba778d2f03
vaishnavibhalerao9405 <vaishnavibhalerao9405@gmail.com>
2026-01-15 09:26:28 +0530

    Add files via upload

---
+++ Assi1/setB/resource.c
@@ -0,0 +1,174 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, n, r;
+    int alloc[10][10], max[10][10], need[10][10];
+    int av[10], finish[10] = {0}, safeSeq[10];
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter number of resources: ");
+    scanf("%d", &r);
+
+    printf("Enter Allocation matrix:\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < r; j++)
+            scanf("%d", &alloc[i][j]);
+
+    printf("Enter Max matrix:\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < r; j++)
+            scanf("%d", &max[i][j]);
+
+    printf("Enter Available resources:\n");
+    for (j = 0; j < r; j++)
+        scanf("%d", &av[j]);
+
+    /* Calculate Need matrix */
+    for (i = 0; i < n; i++)
+        for (j = 0; j < r; j++)
+            need[i][j] = max[i][j] - alloc[i][j];
+
+    printf("\nNeed matrix:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < r; j++)
+            printf("%d ", need[i][j]);
+        printf("\n");
+    }
+
+    int count = 0;
+
+    /* Safety Algorithm */
+    while (count < n)
+    {
+        int found = 0;
+
+        for (i = 0; i < n; i++)
+        {
+            if (finish[i] == 0)
+            {
+                int canExecute = 1;
+
+                for (j = 0; j < r; j++)
+                {
+                    if (need[i][j] > av[j])
+                    {
+                        canExecute = 0;
+                        break;
+                    }
+                }
+
+                if (canExecute)
+                {
+                    for (j = 0; j < r; j++)
+                        av[j] += alloc[i][j];
+
+                    safeSeq[count++] = i;
+                    finish[i] = 1;
+                    found = 1;
+                }
+            }
+        }
+
+        if (!found)
+        {
+            printf("\nSystem is NOT in safe state");
+            return 0;
+        }
+    }
+
+    printf("\nSystem is in SAFE state\nSafe sequence: ");
+    for (i = 0; i < n; i++)
+        printf("P%d ", safeSeq[i]);
+
+    /* ---------------- RESOURCE REQUEST ---------------- */
+
+    int pid, request[10];
+
+    printf("\n\nEnter process number making request: ");
+    scanf("%d", &pid);
+
+    printf("Enter request vector:\n");
+    for (j = 0; j < r; j++)
+        scanf("%d", &request[j]);
+
+    /* Check request <= need */
+    for (j = 0; j < r; j++)
+    {
+        if (request[j] > need[pid][j])
+        {
+            printf("\nError: Request exceeds maximum need");
+            return 0;
+        }
+    }
+
+    /* Check request <= available */
+    for (j = 0; j < r; j++)
+    {
+        if (request[j] > av[j])
+        {
+            printf("\nResources not available, process must wait");
+            return 0;
+        }
+    }
+
+    /* Pretend allocation */
+    for (j = 0; j < r; j++)
+    {
+        av[j] -= request[j];
+        alloc[pid][j] += request[j];
+        need[pid][j] -= request[j];
+    }
+
+    /* Re-run safety check */
+    for (i = 0; i < n; i++)
+        finish[i] = 0;
+    count = 0;
+
+    while (count < n)
+    {
+        int found = 0;
+
+        for (i = 0; i < n; i++)
+        {
+            if (finish[i] == 0)
+            {
+                int canExecute = 1;
+
+                for (j = 0; j < r; j++)
+                {
+                    if (need[i][j] > av[j])
+                    {
+                        canExecute = 0;
+                        break;
+                    }
+                }
+
+                if (canExecute)
+                {
+                    for (j = 0; j < r; j++)
+                        av[j] += alloc[i][j];
+
+                    safeSeq[count++] = i;
+                    finish[i] = 1;
+                    found = 1;
+                }
+            }
+        }
+
+        if (!found)
+        {
+            printf("\nRequest cannot be granted (UNSAFE state)");
+            return 0;
+        }
+    }
+
+    printf("\nRequest GRANTED\nNew Safe sequence: ");
+    for (i = 0; i < n; i++)
+        printf("P%d ", safeSeq[i]);
+
+    return 0;
+}
